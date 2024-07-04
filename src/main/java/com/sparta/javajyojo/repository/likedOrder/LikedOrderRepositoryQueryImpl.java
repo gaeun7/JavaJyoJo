564846183d4bddef9e127ae865b4b3c6fec75a64
equis3351 <130730656+equis3351@users.noreply.github.com>
2024-07-05 01:47:02 +0900

    🐛 bugfix : QueryImpl

--- src/main/java/com/sparta/javajyojo/repository/likedOrder/LikedOrderRepositoryQueryImpl.java
+++ src/main/java/com/sparta/javajyojo/repository/likedOrder/LikedOrderRepositoryQueryImpl.java
@@ -1,13 +1,26 @@
 package com.sparta.javajyojo.repository.likedOrder;
 
 import com.querydsl.jpa.impl.JPAQueryFactory;
+import com.sparta.javajyojo.entity.Review;
+import com.sparta.javajyojo.repository.likedReview.LikedReviewRepositoryQuery;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 @RequiredArgsConstructor
-public class LikedOrderRepositoryQueryImpl {
+public class LikedOrderRepositoryQueryImpl implements LikedReviewRepositoryQuery {
 
     private final JPAQueryFactory queryFactory;
 
+    @Override
+    public List<Review> findLikedReviewsByUser(Long id, int limit) {
+        return List.of();
+    }
+
+    @Override
+    public long countReviewLikesByUserId(Long id) {
+        return 0;
+    }
 }
